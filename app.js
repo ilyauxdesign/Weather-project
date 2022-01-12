0b19458d3a88f32cf28ef8cfe421940fe556b266
“ilyauxdesign” <ilyakalinkin@gmail.com>
2022-01-12 13:27:31 +0200

    js & html added

---
+++ app.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', cityWeather)
+
+function weatherDataFetch(city) {
+    var key = 'b2942e173241588e7bfa51ad9a15fc6a';
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`).then(function(resp) {
+        return resp.json()
+    }) // Convert data to json
+        .then(function(data) {
+            console.log(data) ;
+            drawWeather(data);
+        })
+        .catch(function() {
+            // catch any errors
+    });
+}
+
+function cityWeather(e) {
+    weatherDataFetch('Tallinn' );
+}
+
+function drawWeather( data ) {
+    var celcius = Math.round(parseFloat(data.main.temp)-273.15);
+    var description = data.weather[0].description;
+
+    document.querySelector('#description').innerHTML = description;
+    document.querySelector('#temp').innerHTML = celcius + '&deg;';
+    document.querySelector('#location').innerHTML = data.name;
+}
