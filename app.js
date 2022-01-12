c70c3593f5ac8bbf63f8f64cab6e195810104d45
“ilyauxdesign” <ilyakalinkin@gmail.com>
2022-01-12 13:33:56 +0200

    Set city to LS and get changed weather data from API

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
+// LS object
+const ls = new LS()
+// init city data
+const initCity = ls.getCity()
 // Weather object
-const weather = new Weather('Tallinn')
+const weather = new Weather('initCity')
 
 //UI object
 const ui = new UI()
+
+// events
+const form = document.querySelector('#change-city')
+form.addEventListener('submit', changeWeather)
+
+// change weather
+function changeWeather(event) {
+    const city = document.querySelector('#city-name').value
+    weather.changeCity(city)
+    ls.setCity(city)
+    getWeather()
+    document.querySelector('#city-name').value = ''
+    event.preventDefault()
+}
 
 
 // get city weather
